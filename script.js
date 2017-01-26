20c8f62e23b3e7433bfd5241ede0a28c6210689e
as-f <as-f@users.noreply.github.com>
2017-01-26 15:37:50 -0600

    double viewport size

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function resize() {
     const width = window.innerWidth;
     const height = window.innerHeight;
 
-    const viewBox = [-width / 2, -height / 2, width, height].join(' ');
+    const viewBox = [-width, -height, 2 * width, 2 * height].join(' ');
 
     $plane.setAttribute('width', width);
     $plane.setAttribute('height', height);
